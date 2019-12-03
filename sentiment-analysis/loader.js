714ec527832ecae20cc3ff83ede0c3189fc0f5f6
Anuj Dutt <anuj.dutt9@gmail.com>
2019-12-03 14:19:58 -0500

    Update loader.js

--- sentiment-analysis/loader.js
+++ sentiment-analysis/loader.js
@@ -13,11 +13,11 @@ async function get_review_data(what, where) {
   // Get the review data for restaurants.
   // Learn more at: https://docs.citygridmedia.com/display/citygridv2/Reviews+API
   try {
-    var url="https://api.citygridmedia.com/content/reviews/v2/search/";
+    var url="https://cors-anywhere.herokuapp.com/https://api.citygridmedia.com/content/reviews/v2/search/";
     var params="where?where="+where+"&what="+what+"&publisher=test&format=json&tag=1722";
     var url=encodeURI(url+params);
     console.log(url)
-    var reviews_json = await fetch(url, {mode: 'no-cors'});
+    var reviews_json = await fetch(url);
     console.log(reviews_json)
     var reviews = await reviews_json.json();
     console.log(reviews)
