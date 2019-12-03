7954e777199adbb3e38a51f83a4693cb462d5e2a
Anuj Dutt <anuj.dutt9@gmail.com>
2019-12-03 14:21:44 -0500

    Solved CORS Issue
    Add "https://cors-anywhere.herokuapp.com/" before URL to solve for CORS Blocking issue.

--- sentiment-analysis/loader.js
+++ sentiment-analysis/loader.js
@@ -13,6 +13,7 @@ async function get_review_data(what, where) {
   // Get the review data for restaurants.
   // Learn more at: https://docs.citygridmedia.com/display/citygridv2/Reviews+API
   try {
+    // Add "https://cors-anywhere.herokuapp.com/" before URL to solve for CORS Blocking issue.
     var url="https://cors-anywhere.herokuapp.com/https://api.citygridmedia.com/content/reviews/v2/search/";
     var params="where?where="+where+"&what="+what+"&publisher=test&format=json&tag=1722";
     var url=encodeURI(url+params);
